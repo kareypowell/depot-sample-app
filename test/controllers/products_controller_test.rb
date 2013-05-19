24654facadef933d6d46227ed45b8a0be87821af
Karey Powell <kareyp9@gmail.com>
2013-05-19 05:00:28 -0400

    Add test for products index layout

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,11 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+
+    assert_select '#main h1', 'Listing products'
+    assert_select '#main img.list_image', 3
+    assert_select '#main .list_description dt', 3
+    assert_select '#main .list_description dd', 3
   end
 
   test "should get new" do
