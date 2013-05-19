7b044ef9d709f39efbfcf650f1526a0591744478
Karey Powell <kareyp9@gmail.com>
2013-05-19 04:30:58 -0400

    Add layout for store front and functional test to store controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,15 @@ class Product < ActiveRecord::Base
 
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-	validates :title, uniqueness: true
+	validates :title, uniqueness: true, length: { minimum: 10 }
 
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: "must be a URL for GIF, JPEG or PNG image."
 	}
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
 
 end
