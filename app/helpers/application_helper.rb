dfe2841041269d16e1365cd4814ff4e5aaf07957
Karey Powell <kareyp9@gmail.com>
2013-05-27 18:38:36 -0400

    Add Ajax support for the cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	# Hide the cart if there are no items
+	def hidden_div_if(condition, attributes = {}, &block)
+		if condition
+			attributes["style"] = "display: none"
+		end
+		content_tag("div", attributes, &block)
+	end
+	
 end
