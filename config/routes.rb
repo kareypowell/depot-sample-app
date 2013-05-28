3107224507c109f071ada7900095971739e744b4
Karey Powell <kareyp9@gmail.com>
2013-05-28 00:32:50 -0400

    Add cart checkout feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Depot::Application.routes.draw do
   
+  resources :orders
+
   resources :line_items do
-  	post 'decrement', on: :member
-  	post 'increment', on: :member
+  	post :decrement, on: :member
+  	post :increment, on: :member
   end
 
   resources :carts
-  resources :products
 
-  get "store/index"
+  resources :products do
+  	get :who_bought, on: :member
+  end
   
   root 'store#index', as: 'store'
 end
